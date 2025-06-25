5a466b65f344c1352c349987ac6d6322a21738d4
Arif Rahman <arifrahman1997.aa@gmail.com>
2025-06-25 11:32:07 +0700

    fix: validasi param laporan

--- src/validations/laporan-validation.js
+++ src/validations/laporan-validation.js
@@ -1,20 +1,23 @@
 import { z } from "zod";
 import PageValidation from "./page-validation.js";
 
-export default class Laporanvalidation{
-    static GET_KUNJUNGAN = z.object({
-        faskes_uuid : z.string().min(1).max(255),
-        start_date : z.string().min(1).max(255).optional(),
-        end_date : z.string().min(1).max(255).optional(),
-        search : z.string().min(1).max(255).optional(),
-        pelayanan : z.enum(["rajal", "ranap","igd","aps"]).optional(),
-        ...PageValidation.PAGINATION.shape
-    })
-    static GET_REKAP_KUNJUNGAN = z.object({
-        faskes_uuid : z.string().min(1).max(255),
-        start_date : z.string().min(1).max(255).optional(),
-        end_date : z.string().min(1).max(255).optional(),
-        search : z.string().min(1).max(255).optional(),
-        ...PageValidation.PAGINATION.shape
-    })
+export default class Laporanvalidation {
+  static GET_KUNJUNGAN = z.object({
+    faskes_uuid: z.string().min(1).max(255),
+    start_date: z.string().min().max(255).optional(),
+    end_date: z.string().min().max(255).optional(),
+    search: z.string().min().max(255).optional(),
+    pelayanan: z
+      .string()
+      .transform((val) => (val === "" ? null : val))
+      .pipe(z.enum(["rajal", "ranap", "igd", "aps"]).nullable().optional()),
+    ...PageValidation.PAGINATION.shape,
+  });
+  static GET_REKAP_KUNJUNGAN = z.object({
+    faskes_uuid: z.string().min(1).max(255),
+    start_date: z.string().min().max(255).optional(),
+    end_date: z.string().min().max(255).optional(),
+    search: z.string().min().max(255).optional(),
+    ...PageValidation.PAGINATION.shape,
+  });
 }
