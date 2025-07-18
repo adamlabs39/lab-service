f0bf7219621c31486dab8dc834d6fd957fc8d4d8
engineer-adam <engineer@adamlabs.id>
2025-07-18 14:11:58 +0700

    fix: datamaster item pemeriksaan

--- src/controllers/item-pemeriksaan-controller.js
+++ src/controllers/item-pemeriksaan-controller.js
@@ -50,9 +50,9 @@ export default class ItemPemeriksaanController {
 
   static async findAll(req, res, next) {
     try {
-      request.body.name = request.query.name;
-      request.body.page = request.query.page;
-      request.body.limit = request.query.limit;
+      req.body.name = req.query.name;
+      req.body.page = req.query.page;
+      req.body.limit = req.query.limit;
       req.body.faskes_uuid = req.author.faskesUuid;
       const itemPemeriksaan = await ItemPemeriksaanService.findAll(req.body);
       res
