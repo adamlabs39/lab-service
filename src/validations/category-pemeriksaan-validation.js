912fb5f22c0d9d35932a76dd74ee8919e6c9331e
engineer-adam <engineer@adamlabs.id>
2025-07-23 17:07:35 +0700

    fix: validasi no urut kategori pemeriksaan

--- src/validations/category-pemeriksaan-validation.js
+++ src/validations/category-pemeriksaan-validation.js
@@ -27,7 +27,9 @@ export default class CategoryPemeriksaanValidation {
         invalid_type_error:
           "Data gagal disimpan, No. Urut harus bernilai angka",
       })
-      .min(1),
+      .min(1, {
+        message: "No. urut harus lebih dari 0",
+      }),
     status: z.boolean(),
     faskes_uuid: z.string().min(1).max(255),
   });
@@ -35,9 +37,34 @@ export default class CategoryPemeriksaanValidation {
   static IMPORT = z.array(this.CREATE);
 
   static UPDATE = z.object({
-    code: z.string().min(1).max(255).toUpperCase(),
-    name: z.string().min(1).max(255),
-    no_urut: z.number().min(1),
+    code: z
+      .string({
+        required_error: "Data gagal disimpan, Code harus diisi",
+        invalid_type_error: "Data gagal disimpan, Code harus bernilai string",
+      })
+      .min(1, {
+        message: "Data gagal disimpan, Code harus diisi",
+      })
+      .max(255)
+      .toUpperCase(),
+    name: z
+      .string({
+        required_error: "Data gagal disimpan, Name harus diisi",
+        invalid_type_error: "Data gagal disimpan, Name harus bernilai string",
+      })
+      .min(1, {
+        message: "Data gagal disimpan, Name harus diisi",
+      })
+      .max(255),
+    no_urut: z
+      .number({
+        required_error: "Data gagal disimpan, No. Urut harus diisi",
+        invalid_type_error:
+          "Data gagal disimpan, No. Urut harus bernilai angka",
+      })
+      .min(1, {
+        message: "No. urut harus lebih dari 0",
+      }),
     status: z.boolean(),
     faskes_uuid: z.string().min(1).max(255),
   });
